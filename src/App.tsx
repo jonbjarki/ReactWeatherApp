7bc25f83aa4d966ac98c396cdb60a567f41786dd
jonbg22 <jonbg22@ru.is>
2023-02-06 12:58:08 +0000

    prototype works properly

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { useEffect } from "react";
 function App() {
 
   return (
-    <div className="App">
+    <div id="App">
       <Temperature />
     </div>
   )
--- src/Temperature.tsx
+++ src/Temperature.tsx
@@ -5,7 +5,7 @@ const APIKEY = "c0bd32b3ab26640600d6f7e7219aa84d"
 interface TemperatureObject {
   cityName: string,
   imgSrc: string,
-  temp: number,
+  temp: number | null,
   feelsLike: number,
   tempMin: number,
   tempMax: number
@@ -17,7 +17,7 @@ export default class Temperature extends React.Component <{}, TemperatureObject>
     this.state = {
         cityName: "",
         imgSrc: "",
-        temp: 0,
+        temp: null,
         feelsLike: 0,
         tempMin: 0,
         tempMax: 0
@@ -25,18 +25,29 @@ export default class Temperature extends React.Component <{}, TemperatureObject>
   };
 
   render() {
-    return (
-      <div id="temperatureContainer">
+    let content: JSX.Element = (
+      <div>Loading...</div>
+    );
+    if (this.state.temp !== null) {
+      content = (
+        <div id="temperatureContainer">
+        <h1 id="cityName">{this.state.cityName}</h1>
         <img src={this.state.imgSrc} alt="Image for weather illustration" id="weatherImg" />
-        <p id="temperature">{this.state.temp}°C</p>
+        <p id="temperature">{Math.round(this.state.temp)}°C</p>
+        <p id="feelsLike">Feels Like {Math.round(this.state.feelsLike)}°C</p>
       </div>
+      )
+    }
+    return (
+      content
     );
    }
 
+  // Gets the users location and passes that to the getTemparature() function
   getLocation(): void {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((pos) => {
-        this.getTemperature(pos.coords.longitude, pos.coords.latitude)
+        this.getTemperature(pos.coords.longitude, pos.coords.latitude);
       }, (err) => {
         console.error(err);
       });
@@ -45,6 +56,8 @@ export default class Temperature extends React.Component <{}, TemperatureObject>
     }
   }
   
+
+  // Takes in the users latitude and longitude, makes an API call to fetch weather info for that location and passes that to setTemperature()
   getTemperature(lon: number,lat: number): void {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric`).then((res) => {
     res.json().then(json => {
@@ -53,12 +66,13 @@ export default class Temperature extends React.Component <{}, TemperatureObject>
   })
   }
 
+  // Takes in JSON Response from API call and updates the temperature info
   setTemperature(jsonRes: any): void {
     console.log(jsonRes)
     this.setState({
         cityName: jsonRes.name,
         imgSrc: `https://openweathermap.org/img/wn/${jsonRes.weather[0].icon}@2x.png`,
-        temp: Math.round(jsonRes.main.temp),
+        temp: jsonRes.main.temp,
         feelsLike: jsonRes.main.feels_like,
         tempMin: jsonRes.main.temp_min,
         tempMax: jsonRes.main.temp_max

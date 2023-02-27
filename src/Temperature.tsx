dcc47a8a621e21a7f9ab582b46c3377e6fd25589
jonbg22 <jonbg22@ru.is>
2023-02-27 10:47:19 +0000

    added weather description + css tweaks

--- src/Temperature.tsx
+++ src/Temperature.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 
-const APIKEY = "c0bd32b3ab26640600d6f7e7219aa84d"
+const APIKEY: string = "c0bd32b3ab26640600d6f7e7219aa84d"
+const YELLOW: string = "#fcff82"
+const BLUE: string = "#afc5ff"
+
 
 interface TemperatureObject {
   cityName: string,
@@ -8,7 +11,17 @@ interface TemperatureObject {
   temp: number | null,
   feelsLike: number,
   tempMin: number,
-  tempMax: number
+  tempMax: number,
+  description: string
+}
+
+const toTitleCase = (str: string): string => {
+  return str.replace(
+    /\w\S*/g,
+    (txt) => {
+      return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+    }
+  );
 }
 
 export default class Temperature extends React.Component <{}, TemperatureObject> {
@@ -20,7 +33,8 @@ export default class Temperature extends React.Component <{}, TemperatureObject>
         temp: null,
         feelsLike: 0,
         tempMin: 0,
-        tempMax: 0
+        tempMax: 0,
+        description: ""
     }
   };
 
@@ -33,6 +47,7 @@ export default class Temperature extends React.Component <{}, TemperatureObject>
         <div id="temperatureContainer">
         <h1 id="cityName">{this.state.cityName}</h1>
         <img src={this.state.imgSrc} alt="Image describing the weather" id="weatherImg" />
+        <p id="description">{this.state.description}</p>
         <p id="temperature">{Math.round(this.state.temp)}°C</p>
         <p id="feelsLike">Feels Like {Math.round(this.state.feelsLike)}°C</p>
       </div>
@@ -66,16 +81,21 @@ export default class Temperature extends React.Component <{}, TemperatureObject>
   })
   }
 
+  setStyle(): void {
+    document.querySelector("body")?.setAttribute("style",`background: ${BLUE}`);
+  }
+
   // Takes in JSON Response from API call and updates the temperature info
   setTemperature(jsonRes: any): void {
-    // console.log(jsonRes)
+    this.setStyle()
     this.setState({
         cityName: jsonRes.name,
         imgSrc: `https://openweathermap.org/img/wn/${jsonRes.weather[0].icon}@2x.png`,
         temp: jsonRes.main.temp,
         feelsLike: jsonRes.main.feels_like,
         tempMin: jsonRes.main.temp_min,
-        tempMax: jsonRes.main.temp_max
+        tempMax: jsonRes.main.temp_max,
+        description: toTitleCase(jsonRes.weather[0].description)
     })
   }
 
